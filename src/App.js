7817cf838c02e387145524590e12d07453b1fe62
Maxim Yakovlev <it.trainee@yandex.ru>
2023-06-29 21:46:17 +0300

    got and used the state from redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import { product } from "./data/data";
+import { useSelector } from "react-redux/es/hooks/useSelector.js";
 import { FilterableProductTable } from "./components/FiterableProductTable.jsx";
 
 export default function App() {
+  const product = useSelector((state) => state.dataLoader);
+
   return <FilterableProductTable products={product} />;
 }
