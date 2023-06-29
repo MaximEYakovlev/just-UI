b95880be9845aac019e83a17d82e2b016a805ae2
Maxim Yakovlev <it.trainee@yandex.ru>
2023-06-29 10:07:23 +0300

    defined initial state (redux)

--- src/dataLoadingSlice.js
+++ src/dataLoadingSlice.js
@@ -1,8 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { product } from "./data/data";
 
 export const dataLoadingSlice = createSlice({
   name: "dataLoader",
-  initialState: {},
+  initialState: product,
   reducers: {},
 });
 
